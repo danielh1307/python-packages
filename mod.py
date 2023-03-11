10b1acb5dfb8351d60d906b7a6a2cf251d312ff3
Daniel <hamm.daniel@gmail.com>
2023-03-11 10:48:35 +0100

    Added packages and modules.

---
+++ mod.py
@@ -0,0 +1,22 @@
+s = "If Comrade Napoleon says it, it must be right"
+a = [100, 200, 300]
+
+
+def foo(arg):
+    print(f"arg = {arg}")
+
+
+class Foo:
+    pass
+
+
+def main():
+    print(s)
+    print(a)
+    foo("quux")
+    x = Foo()
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pkg/__init__.py
@@ -0,0 +1,4 @@
+import pkg.mod1
+
+print("Ah, you imported the package. That's great.")
+A = ["quux", "corge", "grault"]
